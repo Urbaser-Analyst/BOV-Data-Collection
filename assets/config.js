8f2b2eacc273a872446e4dbff28e5069c16648cb
Urbaser-Analyst <analyst.urbaser@gmail.com>
2026-08-19 12:05:41 +0530

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,5 @@
+// ============================================================
+// PASTE YOUR APPS SCRIPT WEB APP URL BELOW after deployment.
+// Apps Script editor > Deploy > New deployment > Web app > Copy the /exec URL
+// ============================================================
+const API_URL = "https://script.google.com/macros/s/AKfycbyRnYx2PH24ke0xuIXgBdZGgrW4BZYuoPRwQNZn8SE4a-rZ1GsONiY3zZfUAiCIJ33w/exec";
